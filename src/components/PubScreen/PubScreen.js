9607367a6b5410df98471bd6c7acd223b1361cdb
Michal Trendel <michaltrendel93@gmail.com>
2019-02-25 15:52:32 +0100

    wip pubscreen

--- src/components/PubScreen/PubScreen.js
+++ src/components/PubScreen/PubScreen.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import SearchBar from "../SearchBar/SearchBar";
 
 import './PubScreen.css'
 
 class PubScreen extends Component {
   render() {
     return (
-      <div className="PubScreen-wrapper">
-        <h1>PUB SCREEN</h1>
-
+      <div>
+        <SearchBar />
+        <div className="PubScreen-wrapper">
+          <h1>PUB SCREEN</h1>
+        </div>
       </div>
     )
   }

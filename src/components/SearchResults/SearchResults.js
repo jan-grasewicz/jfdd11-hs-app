84823ce63f44b727ca6eb0abade35d507c04aa0f
Michal Trendel <michaltrendel93@gmail.com>
2019-02-25 15:35:48 +0100

    WIP state fixed

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -5,17 +5,10 @@ import SearchBar from "../SearchBar/SearchBar";
 
 class SearchResults extends Component {
   state = {
-    publist: []
+    publist: this.props.pubs
   };
 
-  componentDidMount() {
-    fetch(process.env.PUBLIC_URL + "/data/publist.json")
-      .then(data => data.json())
-      .then(stuff =>
-        Object.entries(stuff).map(([id, value]) => ({ id, ...value }))
-      )
-      .then(publist => this.setState({ publist }));
-  }
+
 
   render() {
     console.log(this.state.publist);

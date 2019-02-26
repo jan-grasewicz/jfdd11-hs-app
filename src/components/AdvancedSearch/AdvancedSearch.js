cbc94ae05a1f82a43deb0b1ab1c412608928cbd2
Robert Dopieralski <abc507446692@gmail.com>
2019-02-26 15:58:14 +0100

    Opening and closing hours are generated due to database

--- src/components/AdvancedSearch/AdvancedSearch.js
+++ src/components/AdvancedSearch/AdvancedSearch.js
@@ -6,11 +6,11 @@ class AdvancedSearch extends Component {
   state = {
     publist: this.props.pubs,
     city: "all",
-    cout: 0
-  };
-
-  handleCitySelect = event => {
-    this.setState({ city: event.target.value });
+    cout: 5,
+    openedFrom: "",
+    openedTill: "",
+    openButtons: [],
+    closeButtons: []
   };
 
   handleChange = event => {
@@ -22,6 +22,39 @@ class AdvancedSearch extends Component {
     console.log("submitted " + this.state.city);
   };
 
+  checkClosingHours = () => {
+    let closinghours = [];
+    Object.entries(this.state.publist)
+      .map(([id, value]) => ({ id, ...value }))
+      .forEach(pub => {
+        if (closinghours.includes(pub.closehour)) {
+          return;
+        } else {
+          closinghours.push(pub.closehour);
+        }
+      });
+    this.setState({ closeButtons: closinghours });
+  };
+
+  checkOpeningHours = () => {
+    let openinghoursarr = [];
+    Object.entries(this.state.publist)
+      .map(([id, value]) => ({ id, ...value }))
+      .forEach(pub => {
+        if (openinghoursarr.includes(pub.openhour)) {
+          return;
+        } else {
+          openinghoursarr.push(pub.openhour);
+        }
+      });
+    this.setState({ openButtons: openinghoursarr });
+  };
+
+  componentDidMount() {
+    this.checkOpeningHours();
+    this.checkClosingHours();
+  }
+
   render() {
     console.log(this.state);
     return (
@@ -30,10 +63,12 @@ class AdvancedSearch extends Component {
           <form>
             <div>
               <label name="city">City </label>
-              <select onChange={this.handleCitySelect}>
-                <option selected value="all">
-                  Wszystkie Miasta
-                </option>
+              <select
+                name="city"
+                defaultValue="all"
+                onChange={this.handleChange}
+              >
+                <option value="all">Wszystkie Miasta</option>
                 <option value="Gdańsk">Gdańsk</option>
                 <option value="Sopot">Sopot</option>
                 <option value="Gdynia">Gdynia</option>
@@ -48,6 +83,37 @@ class AdvancedSearch extends Component {
                 name="cout"
                 onChange={this.handleChange}
               />
+            </div>
+            <div>
+              <label>Opened from:</label>
+              <select
+                name="openedFrom"
+                defaultValue={"all"}
+                onChange={this.handleChange}
+              >
+                <option value="all">All</option>
+                {this.state.openButtons.map(hour => (
+                  <option key={hour} value={hour}>
+                    {hour}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label>Opened till:</label>
+              <select
+                name="openedTill"
+                defaultValue={"all"}
+                onChange={this.handleChange}
+              >
+                <option value="all">All</option>
+
+                {this.state.closeButtons.map(hour => (
+                  <option key={hour} value={hour}>
+                    {hour}
+                  </option>
+                ))}
+              </select>
             </div>
             <button onClick={this.submitForm}> Submit</button>
           </form>

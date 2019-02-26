0470d1780833995177f248fe97f934baec37a86e
Michal Trendel <michaltrendel93@gmail.com>
2019-02-26 16:00:10 +0100

    wip pubScreen structure

--- src/components/PubScreen/PubScreen.js
+++ src/components/PubScreen/PubScreen.js
@@ -8,7 +8,21 @@ class PubScreen extends Component {
     return (
       <div>
         <div className="PubScreen-wrapper">
-          <h1>PUB SCREEN</h1>
+          <div className="PubScreen-img-wrapper">
+            <img className="PubScreen-pubImg"
+              src="#"
+              alt={`pub`} ></img>
+          </div>
+          <h1 className="PubScreen-pubName">nazwa pubu tutaj</h1>
+          <div className="PubScreen-info-wrapper">
+            <span className="PubScreen-infoTitle">City:</span><p>Wejherowo</p>
+            <span className="PubScreen-infoTitle">Adress:</span><p>62 Drunk Street</p>
+            <span className="PubScreen-infoTitle">Operating hours: <p> from 18:00 to 02:30</p></span>
+            <span className="PubScreen-infoTitle">Phone:</span><p>(+48) 666-666-666</p>
+            <span className="PubScreen-infoTitle">email:</span><p>pub@wejherowo.com</p>
+            <span className="PubScreen-infoTitle">email:</span><p>lorem impsum dolor sit lorem impsum dolor sit lorem impsum dolor sit lorem impsum dolor sitlorem impsum dolor sit lorem impsum dolor sit lorem impsum dolor sit lorem impsum dolor sit</p>
+          </div>
+
         </div>
       </div>
     )

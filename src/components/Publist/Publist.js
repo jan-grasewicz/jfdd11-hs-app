2826608a89c62700bc53e50d595d48277cea39c8
jan-grasewicz <j.grasewicz@gmail.com>
2019-02-27 15:09:46 +0100

    Link on whole pub list item

--- src/components/Publist/Publist.js
+++ src/components/Publist/Publist.js
@@ -9,24 +9,22 @@ class Publist extends Component {
         <h1>Publist</h1>
         <ul className="Publist-ul">
           {this.props.publistdata.map(pub => (
-            //
-            <li key={pub.id} className="Publist-li">
-              <h2 className="Publist-h2">
-                <Link to={`/publist/${pub.id}`}>{pub.name}</Link>
-              </h2>
-
-              <div className="Publist-imgcontainer">
-                <img className="Publist-img" src={pub.img} />
-              </div>
-              <div className="Publist-info">
-                <p>{pub.city}</p>
-                <p>{pub.address}</p>
-                <p>
-                  Open from {pub.openhour} to {pub.closehour}
-                </p>
-                {/* Add banner opened now or closed now instead of hours */}
-              </div>
-            </li>
+            <Link key={pub.id} to={`/publist/${pub.id}`}>
+              <li  className="Publist-li">
+                <h2 className="Publist-h2">{pub.name}</h2>
+                <div className="Publist-imgcontainer">
+                  <img className="Publist-img" src={pub.img} />
+                </div>
+                <div className="Publist-info">
+                  <p>{pub.city}</p>
+                  <p>{pub.address}</p>
+                  <p>
+                    Open from {pub.openhour} to {pub.closehour}
+                  </p>
+                  {/* Add banner opened now or closed now instead of hours */}
+                </div>
+              </li>
+            </Link>
           ))}
         </ul>
       </div>

613917dd4709a2b6b310200f630acf8f480b94ab
Robert Dopieralski <abc507446692@gmail.com>
2019-03-04 15:29:06 +0100

    added searchphrase to check cities

--- src/components/PubScreen/PubScreen.js
+++ src/components/PubScreen/PubScreen.js
@@ -2,63 +2,69 @@ import React, { Component } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 import "./PubScreen.css";
+import { withAdvancedSearch } from "../../contexts/AdvancedSearch/AdvancedSearch";
 
 class PubScreen extends Component {
   render() {
+    let pubId = this.props.match.params.pubId;
+    let pub = this.props.advancedSearchContext.publist.find(
+      pub => pub.id === pubId
+    );
+    console.log(pub);
     return (
       <div>
-        <div className="PubScreen-wrapper">
-          <div className="PubScreen-img-wrapper">
-            <img className="PubScreen-pubImg" src="#" alt={`pub`} />
-          </div>
-          <h1 className="PubScreen-pubName">Nazwa Pubu</h1>
-          <div className="PubScreen-info-wrapper">
-            <dl className="PubScreen-info">
-              <dt>City:</dt>
-              <dd>Wejherowo</dd>
-              <br />
-              <dt>Adress:</dt>
-              <dd>62 Drunk Street</dd>
-              <br />
-              <dt>Operating hours:</dt>
-              <dd>18:00 - 02:30</dd>
-              <br />
-              <dt>Available space:</dt>
-              <dd>17</dd>
-              <br />
-              <dt>Phone:</dt>
-              <dd>(+48) 666-666-666</dd>
-              <br />
-              <dt>About us:</dt>
-              <br />
-              <dd className="PubScreen-info-about">
-                lorem impsum dolor sit lorem impsum dolor sit lorem impsum dolor
-                sit lorem impsum dolor sitlorem impsum dolor sit lorem impsum
-                dolor sit lorem impsum dolor sit lorem impsum dolor sit
-              </dd>
-            </dl>
-          </div>
-          <p className="PubScreen-find">Find us here:</p>
+        {pub && (
+          <div className="PubScreen-wrapper">
+            <div className="PubScreen-img-wrapper">
+              <img className="PubScreen-pubImg" src={pub.img} alt={`pub`} />
+            </div>
+            <h1 className="PubScreen-pubName">{pub.name}</h1>
+            <div className="PubScreen-info-wrapper">
+              <dl className="PubScreen-info">
+                <dt>City:</dt>
+                <dd>{pub.city}</dd>
+                <br />
+                <dt>Adress:</dt>
+                <dd>{pub.address}</dd>
+                <br />
+                <dt>Operating hours:</dt>
+                <dd>
+                  {pub.openhour} - {pub.closehour}
+                </dd>
+                <br />
+                <dt>Available space:</dt>
+                <dd>{pub.space}</dd>
+                <br />
+                <dt>Phone:</dt>
+                <dd>{pub.phone}</dd>
+                <br />
+                <dt>About us:</dt>
+                <br />
+                <dd className="PubScreen-info-about">{pub.about}</dd>
+              </dl>
+            </div>
+            <p className="PubScreen-find">Find us here:</p>
 
-          <Map
-            center={[54.759159, 18.508961]}
-            zoom={13}
-            style={{ height: 300, width: 375 }}
-          >
-            <TileLayer
-              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[54.7591598, 18.508961]}>
-              <Popup>
-                Wyjdziesz stąd najebany jak księciunio <br /> Drogo, ale dobrze.
-              </Popup>
-            </Marker>
-          </Map>
-        </div>
+            <Map
+              center={[pub.coordinates.latitude, pub.coordinates.longitude]}
+              zoom={13}
+              style={{ height: 300, width: 375 }}
+            >
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker
+                position={[pub.coordinates.latitude, pub.coordinates.longitude]}
+              >
+                <Popup>{pub.email}</Popup>
+              </Marker>
+            </Map>
+          </div>
+        )}
       </div>
     );
   }
 }
 
-export default PubScreen;
+export default withAdvancedSearch(PubScreen);

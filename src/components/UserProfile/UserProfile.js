16d906a0c59226e0619ca4de20a6b332f0ed7571
jan-grasewicz <j.grasewicz@gmail.com>
2019-03-06 17:00:08 +0100

    retrive data from contexts

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 
 import "./UserProfile.css";
+import { withAdvancedSearch } from "../../contexts/AdvancedSearch/AdvancedSearch";
+import { withAuth } from "../../contexts/AuthContext/AuthContext";
 
 class UserProfile extends Component {
   render() {
+    let { user } = this.props.authContext;
+    console.log(user);
     return (
       <div className="UserProfile">
-        <h2>UserProfile</h2>
+        <div className="UserProfile-">
+          <h2>UserName</h2>
+          <p>e-mail: {user && user.email}</p>
+        </div>
       </div>
     );
   }
 }
 
-export default UserProfile;
+export default withAuth(withAdvancedSearch(UserProfile));

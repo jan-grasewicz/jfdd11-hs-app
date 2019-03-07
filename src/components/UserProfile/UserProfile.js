ee3c1ca442577ddbde1329f0fe34140a214df307
jan-grasewicz <j.grasewicz@gmail.com>
2019-03-07 16:25:35 +0100

    basic css for UserProfile

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -13,14 +13,31 @@ class UserProfile extends Component {
     return (
       <div className="UserProfile">
         <div className="UserProfile-data">
-          <img src="http://placeimg.com/200/250/people" />
-          <div>
+          <img
+            className="UserProfile-img"
+            src="http://placeimg.com/120/150/people"
+          />
+          <div className="UserProfile-info">
             <h2>{userData && userData.name}</h2>
             <h2>{userData && userData.surname}</h2>
             <p>e-mail: {user && user.email}</p>
+            <p>phone: {userData && userData.phone}</p>
+            <button>Edit profile</button>
           </div>
         </div>
-        <div className="UserProfile-reservations" />
+        <div className="UserProfile-reservations">
+          <ul>
+            <li>There should be a list of all reservations</li>
+            <li>
+              sorted the way we have "pending" on top. Then "accepted". Then
+              "past".
+            </li>
+            <li>
+              If the User is also a pub owner then display second tab named: "My
+              pubs".
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }

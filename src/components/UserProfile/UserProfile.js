0874a71ebb77f1f2497ff81c8f38fa46317ba3d1
jan-grasewicz <j.grasewicz@gmail.com>
2019-03-07 15:57:17 +0100

    userData added to AuthContext, data displayed in user profile

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -6,14 +6,21 @@ import { withAuth } from "../../contexts/AuthContext/AuthContext";
 
 class UserProfile extends Component {
   render() {
-    let { user } = this.props.authContext;
-    console.log(user);
+    let { user, userData } = this.props.authContext;
+
+    // console.log("adv", this.props.advancedSearchContext);
+    console.log("auth", this.props.authContext);
     return (
       <div className="UserProfile">
-        <div className="UserProfile-">
-          <h2>UserName</h2>
-          <p>e-mail: {user && user.email}</p>
+        <div className="UserProfile-data">
+          <img src="http://placeimg.com/200/250/people" />
+          <div>
+            <h2>{userData && userData.name}</h2>
+            <h2>{userData && userData.surname}</h2>
+            <p>e-mail: {user && user.email}</p>
+          </div>
         </div>
+        <div className="UserProfile-reservations" />
       </div>
     );
   }

35a1489c647ed662afeffc04deae579690bb458e
jan-grasewicz <j.grasewicz@gmail.com>
2019-03-08 16:59:51 +0100

    wip

--- src/components/ProfileTabs/ProfileTabs.js
+++ src/components/ProfileTabs/ProfileTabs.js
@@ -4,12 +4,19 @@ import "./ProfileTabs.css";
 import Reservations from "../Reservations";
 
 class ProfileTabs extends Component {
+  state = {
+    tab: "reservations"
+  };
   render() {
     return (
       <div className="ProfileTabs">
         <h2>ProfileTabs</h2>
-        <button>Reservations</button>
-        <button>My Pubs</button>
+        <button onClick={() => this.setState({ tab: "reservations" })}>
+          Reservations
+        </button>
+        <button onClick={() => this.setState({ tab: " mypubs" })}>
+          My Pubs
+        </button>
         <Reservations />
       </div>
     );

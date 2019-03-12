852e6d3e335b0d1712f518cdc3ab94a032a1c4f8
Jan Grasewicz <j.grasewicz@gmail.com>
2019-03-12 21:13:05 +0100

    basic CSS

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -7,23 +7,24 @@ class UserProfile extends Component {
   render() {
     let { user, userData } = this.props.authContext;
     return (
+      <div className='UserProfile-background'>
       <div className="UserProfile">
         <div className="UserProfile-data">
           <div className="UserProfile-img">
             <img alt="Profile" src="http://placeimg.com/120/160/people" />
           </div>
           <div className="UserProfile-info">
-            <h2 >{userData && userData.name}</h2>
+            <h2 className="UserProfile-name">{userData && userData.name}</h2>
             <h2>{userData && userData.surname}</h2>
             <p>e-mail: {user && user.email}</p>
             <p>phone: {userData && userData.phone}</p>
           </div>
           <button>edit</button>
         </div>
-        <div className="UserProfile-tabs">
+        
           <ProfileTabs />
-        </div>
-      </div>
+        
+      </div></div>
     );
   }
 }

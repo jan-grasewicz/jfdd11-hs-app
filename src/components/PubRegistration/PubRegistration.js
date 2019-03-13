bcc6976d8821724b3b1c673248563a9e69006f64
Michał Trendel <michaltrendel93@gmail.com>
2019-03-13 15:23:28 +0100

    WIP added leaflet map

--- src/components/PubRegistration/PubRegistration.js
+++ src/components/PubRegistration/PubRegistration.js
@@ -1,9 +1,33 @@
+// pytania do Mistrza :
+// - czy atrybuty name musza byc identycznie nazwane jak w firebase w liscie pubow?
+// - czy open-close hours moga byc w inpucie time a nie select?
+// - jak to wgl wjebac do firebomby?
+// - jak dodac lokalizacje pubow?
+//        * (2 inputy i sztywno wklepac lang i long + ew. link do strony ktora to umozliwia)
+//        * wrzucic mapke leaflet i jakims cudem umozliwic dodanie znacznika uzytkownikowi i po wybraniu tego wlasnie znacznika
+//          na mapie, jakims magicznym sposobem te wartosci znajda sie w koordynatach pubu i tez są przeslane do firebomby razem z formularzem
+// - jak napisac, ze wszystkie pola są wymagane ? (nie chodzi o dopisanie required ale o tą chujową gwiazdeczkę - przy każdym input'ie czy zebrac wszystko i dupnąć info ze musi wypełnić)
+
 import React, { Component } from "react";
 import firebase from "firebase";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 import "./PubRegistration.css";
 
 class PubRegistration extends Component {
+  state = {
+    name: "",
+    city: "",
+    adress: "",
+    openhour: "",
+    closehour: "",
+    email: "",
+    space: "",
+    phone: "",
+    img: "chooy wie",
+    about: ""
+  };
+
   render() {
     return (
       <div className="PubRegistration-container">
@@ -50,8 +74,10 @@ class PubRegistration extends Component {
             type="tel"
             name="phone"
             minLength="9"
-            maxLength="16"
+            maxLength="9"
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}"
           />
+          <span>Format: 123-456-789</span>
           <br />
           <label htmlFor="pub-img">Upload Your Pub Img:</label>
           <input
@@ -73,6 +99,23 @@ class PubRegistration extends Component {
           />
           <input type="submit" value="Submit Request" />
         </form>
+        <div>
+          <span>Latitude: 33.33</span>
+          <span>Longitude: 23.22</span>
+        </div>
+        <Map
+          center={{ lat: 54.372158, lng: 18.638306 }}
+          zoom={13}
+          style={{ height: 300, width: 375 }}
+        >
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={{ lat: 54.372158, lng: 18.638306 }}>
+            <Popup>{555 - 555 - 555}</Popup>
+          </Marker>
+        </Map>
       </div>
     );
   }

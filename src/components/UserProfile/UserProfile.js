4165d0dcaeef94d22b4ae29a554bd5421696122c
jan-grasewicz <j.grasewicz@gmail.com>
2019-03-13 16:06:10 +0100

    reservations css

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -2,29 +2,31 @@ import React, { Component } from "react";
 import ProfileTabs from "../ProfileTabs";
 import "./UserProfile.css";
 import { withAuth } from "../../contexts/AuthContext/AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 
 class UserProfile extends Component {
   render() {
     let { user, userData } = this.props.authContext;
     return (
-      <div className='UserProfile-background'>
-      <div className="UserProfile">
-        <div className="UserProfile-data">
-          <div className="UserProfile-img">
-            <img alt="Profile" src="http://placeimg.com/120/160/people" />
+      <div className="UserProfile-background">
+        <div className="UserProfile">
+          <div className="UserProfile-data">
+            <div className="UserProfile-img">
+              <img alt="Profile" src="http://placeimg.com/120/160/people/5" />
+            </div>
+            <div className="UserProfile-info">
+              <h2 className="UserProfile-name">{userData && userData.name}</h2>
+              <h2>{userData && userData.surname}</h2>
+              <p>e-mail: {user && user.email}</p>
+              <p>phone: {userData && userData.phone}</p>
+            </div>
+            <FontAwesomeIcon icon={faPencilAlt} className="UserProfile-edit" />
           </div>
-          <div className="UserProfile-info">
-            <h2 className="UserProfile-name">{userData && userData.name}</h2>
-            <h2>{userData && userData.surname}</h2>
-            <p>e-mail: {user && user.email}</p>
-            <p>phone: {userData && userData.phone}</p>
-          </div>
-          <button>edit</button>
-        </div>
-        
+
           <ProfileTabs />
-        
-      </div></div>
+        </div>
+      </div>
     );
   }
 }

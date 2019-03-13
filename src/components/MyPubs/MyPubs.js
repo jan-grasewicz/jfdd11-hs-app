ecca4c6eb5a3edd613ac29bc33ce02b805229038
jan-grasewicz <j.grasewicz@gmail.com>
2019-03-13 16:45:18 +0100

    fix css

--- src/components/MyPubs/MyPubs.js
+++ src/components/MyPubs/MyPubs.js
@@ -7,7 +7,24 @@ class MyPubs extends Component {
     return (
       <div className="MyPubs">
         <ul>
-          <li>pub 1</li>
+          {/* map pubs */}
+        <li key="firebaseId1">
+            <div className="MyPubs-pub">
+              <h2 className="MyPubs-pubname">Pub Name</h2>
+              <p className="MyPubs-reservations">
+                reservations:
+              </p>
+              
+              <ul>
+                {/* map filter reservations for one pub */}
+
+                
+              </ul>
+              <p className="MyPubs-date">
+                reservation from: (hour) (date)
+              </p>
+            </div>
+          </li>
         </ul>
       </div>
     );
